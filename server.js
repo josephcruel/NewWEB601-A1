4308f42dc9d8f7e969a43b9f74e9bf874ca479da
Joseph Cruel <Joseph-Cruel@live.nmit.ac.nz>
2024-04-06 17:19:00 +1300

    created new article page with textboxes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
+
+mongoose.connect('mongodb+srv://josephcruel:UkwKD5LJ9yLIWp12@calories.ifsfwtg.mongodb.net/?retryWrites=true&w=majority')
 
 app.set('view engine', 'ejs')
 
@@ -17,7 +20,7 @@ app.get('/', (req, res) => {
         createdAt: new Date(),
         description: 'Test description 2'
     }]
-    res.render('index', { articles: articles })
+    res.render('articles/index', { articles: articles })
 })
 
 app.listen(5000)
